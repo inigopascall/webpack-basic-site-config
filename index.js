ae9d4a405b66c627d2bcc84a91f2ec12b1aaacb3
Inigo Pascall <inigo@inigowebdesign.co.uk>
2019-07-06 20:52:51 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import './dev/scss/main.scss'; // Your main scss file
+
+import './dev/js/main'; // Your main js file
+import './dev/js/plugin'; // any plugins you need (eg put your jQuery plugins here)
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const AssetVersioning = require('webpack-php-asset-versioning');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: './js/main.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 8192,
+                        outputPath: 'images'
+                    },
+                  },
+                ],
+            },
+            {
+                test: /\.(woff|woff2)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 5000,
+                        outputPath: 'fonts'
+                    },
+                  },
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            outputPath: './css/',
+                        }
+                    },
+                    {
+                        loader: 'extract-loader',
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                                    plugins: () => [
+                                        require('cssnano')(),
+                                    ]
+                                }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    output: {
+                        comments: false
+                    },
+                },
+            }),
+        ]
+    },
+    externals: {
+        jquery: 'jQuery'
+    },
+    mode : 'production',
+    watch: true,
+    plugins: [
+        new BrowserSyncPlugin({
+            host: '192.168.10.10', // Vagrant
+            proxy: 'mysite.local',
+            open: false,
+            files: [
+                '../resources/views/**/*.blade.php', // eg. Laravel view files
+            ],
+        }),
+        new AssetVersioning({
+            file_name: '../assets.version.php'
+        })
+    ]
+};
